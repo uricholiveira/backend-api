9a5eb089c054bd5c3b4a3683ce88e0e86af31bbb
Urich Oliveira <oliveira.urich@gmail.com>
2022-06-17 20:33:19 -0300

    added log

--- app/core/settings/__init__.py
+++ app/core/settings/__init__.py
@@ -3,12 +3,14 @@ from pydantic import BaseSettings
 from app.core.settings.app import AppConfig
 from app.core.settings.authentication import AuthConfig
 from app.core.settings.database import DatabaseConfig
+from app.core.settings.util import UtilConfig
 
 
 class Settings(BaseSettings):
     database: DatabaseConfig = DatabaseConfig()
     app: AppConfig = AppConfig()
     auth: AuthConfig = AuthConfig()
+    util: UtilConfig = UtilConfig()
 
 
 settings = Settings()
